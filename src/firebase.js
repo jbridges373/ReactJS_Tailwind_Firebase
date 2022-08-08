d780d3121f1403c34ca2135152851b9671099f34
John Bridges <jbridges373@gmail.com>
2022-08-08 15:34:16 +0100

    firebase auth added

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
